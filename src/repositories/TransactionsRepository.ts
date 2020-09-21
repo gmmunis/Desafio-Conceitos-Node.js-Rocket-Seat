1537d9c0f5ba3adb88eb6f043de09cba6b1c475c
guilherme munis <gmmunis@hotmail.com>
2020-09-21 19:16:34 -0300

    first commit

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,71 @@
+import Transaction from '../models/Transaction';
+import {uuid} from 'uuidv4';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface RequestDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const income = this.transactions.reduce((previous, current) => {
+      if (current.type === 'income') {
+        return previous + current.value;
+      }
+      return previous;
+    }, 0);
+
+    const outcome = this.transactions.reduce((previous, current) => {
+      if (current.type === 'outcome') {
+        return previous + current.value;
+      }
+      return previous;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
+  }
+
+  public create({ title, value, type }: RequestDTO): Transaction {
+
+    const { total } = this.getBalance();
+
+    if(total < value && type === 'outcome') {
+      throw new Error('Insufficient funds!');
+    }
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,28 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, type, value }: RequestDTO): Transaction {
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
